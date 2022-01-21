236744a24e18008e1218149d68b1636c476ee76c
YASSEROVIC <yasserovic@gmail.com>
2022-01-20 18:26:45 -0600

    Finishing Assignment 3
    make classes and use JS to insert to HTML file

--- script-classes.js
+++ script-classes.js
@@ -2,21 +2,29 @@
  * Create a class for the Backpack object type.
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
  */
-import Backpack from "./Backpack.js";
+import univ from "./univ.js";
 
-const everydayPack = new Backpack(
-  "Everyday Backpack",
-  30,
-  "grey",
-  15,
-  26,
-  26,
-  false
+const infoUnv = new univ(
+  "United States",
+  "Texas",
+  "San Antonio",
+  "Univiersity of The Incarnate Word",
+  1881,
+  "uiw.edu",
 );
 
-const content = ``;
+const content = `<main>
+<article>
+  <h1>${infoUnv.univName}</h1>
+  <ul>
+    <li>Country: ${infoUnv.country}</li>
+    <li>State: ${infoUnv.state}</li>
+    <li>City: ${infoUnv.city}</li>
+    <li>Established: ${infoUnv.years}</li>
+    <li>Website: ${infoUnv.link}</li>
+  </ul>
+</article>
+</main>
+`;
 
 document.body.innerHTML = content;
-
-console.log("The everydayPack object:", everydayPack);
-console.log("The pocketNum value:", everydayPack.pocketNum);
